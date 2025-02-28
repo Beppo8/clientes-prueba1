6a9bbb748139662b78492d9deab1adaff447153c
Beppo8 <beppo2488sm@gmail.com>
2025-02-28 09:09:18 -0600

    Se añadio OpenApi y se configuro.

---
+++ src/main/java/com/example/demo/config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Clientes")
+                        .version("1.0.0")
+                        .description("CRUD de clientes utilizando Spring Boot y OpenAPI 2.0"));
+    }
+}
